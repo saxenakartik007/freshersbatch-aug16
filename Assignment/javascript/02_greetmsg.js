e4dabf4bd0993b1cc4083b968738edab21c199cf
Kartik Saxena <saxena_k@India.XoriantCorp.com>
2016-08-18 14:36:13 +0530

    All assignments

--- Assignment/javascript/02_greetmsg.js
+++
@@ -1,9 +0,0 @@
-function greet(){
-	var name;
-	
-	
-		name=document.getElementById('name').value;
-		  alert("Hello "+name);
-	
-	
-}
--- Assignment/javascript/14_charcount.js
+++
@@ -1,9 +0,0 @@
-function checkPC(){
-
-var str=document.getElementById("str").value;
-
-var len = str.length;
-document.getElementById("sol").innerHTML =(str +" is "+len+"character long");
-	
-
-}
